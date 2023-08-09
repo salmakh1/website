06417ce6f74c372c0217843b2c1e36688a55c45b
fouratf <fares.fourati@kaust.edu.sa>
2023-08-09 15:46:21 +0300

    rgl

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -63,7 +63,6 @@ def html_escape(text):
 
 import os
 for row, item in publications.iterrows():
-    
     md_filename = str(item.pub_date) + "-" + item.url_slug + ".md"
     html_filename = str(item.pub_date) + "-" + item.url_slug
     year = item.pub_date[:4]
