c5a4f690a5a8b4467979a03e762e5b399d213d3e
fouratf <fares.fourati@kaust.edu.sa>
2023-08-09 16:27:59 +0300

    papers

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -85,7 +85,7 @@ for row, item in publications.iterrows():
     if len(str(item.paper_url)) > 5:
         md += "\npaperurl: '" + item.paper_url + "'"
     
-    md += "\ncitation: '" + html_escape(item.citation) + "'"
+    # md += "\ncitation: '" + html_escape(item.citation) + "'"
     
     md += "\n---"
     
@@ -97,7 +97,7 @@ for row, item in publications.iterrows():
     if len(str(item.excerpt)) > 5:
         md += "\n" + html_escape(item.excerpt) + "\n"
         
-    md += "\nRecommended citation: " + item.citation
+    md += "\nRef.: " + item.citation
     
     md_filename = os.path.basename(md_filename)
        
